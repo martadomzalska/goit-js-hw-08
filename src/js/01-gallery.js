a4eac384dc97ac71bc41e5f7637e044f7a32a2b2
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-16 00:15:09 +0100

    task-01-update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let gallery = document.querySelector('.gallery');
+for (const item of galleryItems) {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>`
+  );
+}
+gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+function showPic(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  gallery.on('show.simplelightbox', function () {});
+}
+
+gallery.addEventListener('click', showPic);
+
+
+
 
 console.log(galleryItems);
