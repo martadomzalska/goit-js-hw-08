669e54850c5c3aa46eb9a75ba4d69e9940c6397c
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-17 00:08:46 +0100

    task-02-update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) { 
+    
+};
+player.on('timeupdate', onPlay);
+localStorage.setItem('videoplayer-current-time', 'currentTime');
+
+
 
