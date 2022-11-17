ca70076d9f7f4bc05a5030d6e1b1088d43047e56
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-17 18:21:18 +0100

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,38 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function (data) { 
-    
+const onPlay = function (data) {
+        player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
 };
-player.on('timeupdate', onPlay);
-localStorage.setItem('videoplayer-current-time', 'currentTime');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 
 
