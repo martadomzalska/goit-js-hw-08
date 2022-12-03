59474646bca4d541ee59375a32cb56cfca201a24
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-12-03 18:05:42 +0100

    update-task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,33 +5,26 @@ const email = form.querySelector('input[name]');
 const message = form.querySelector('textarea');
 const storageKey = 'feedback-form-state';
 
-form.addEventListener(
-  'input',
-  throttle(event => {
-    const {
-      elements: { email, message },
-    } = event.currentTarget;
-    console.log(email.value, message.value);
-    const formValues = {
-      email: email.value,
-      message: message.value,
-    };
-    localStorage.setItem(storageKey, JSON.stringify(formValues));
-  }, 500)
-);
+form.addEventListener('input', throttle(() => {
+  const formValues = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formValues));
+}, 500));
 
 const json = localStorage.getItem(storageKey);
 if (json !== null) {
   const reload = JSON.parse(json);
-
   email.value = reload.email;
-
   message.value = reload.message;
-}
+};
 
-form.addEventListener(
-  'submit',
-  throttle(event => {
-    localStorage.removeItem(storageKey);
-  }, 500)
-);
+form.addEventListener('submit', throttle((event) => {
+  const savedData = json;
+  console.log(savedData);
+  event.preventDefault();
+  localStorage.removeItem(storageKey);
+  form.reset();
+}, 500));
+  
