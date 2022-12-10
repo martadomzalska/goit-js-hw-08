9ff047faa341928e6b7e26f9ecef6feed85c5117
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-12-10 15:57:12 +0100

    update 03-feedback
    added "disabled" attr to the submit btn

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,12 +4,22 @@ const form = document.querySelector('.feedback-form');
 const email = form.querySelector('input[name]');
 const message = form.querySelector('textarea');
 const storageKey = 'feedback-form-state';
+const button = form.querySelector('button');
+if (message.value === '' || email.value === '') {
+  button.setAttribute('disabled', 'true');
+}
 
 form.addEventListener('input', throttle(() => {
   const formValues = {
     email: email.value,
     message: message.value,
   };
+  if (message.value === "" || email.value === "") {
+     button.setAttribute("disabled", "true");
+  } else {
+     button.removeAttribute('disabled');
+     
+  }
   localStorage.setItem(storageKey, JSON.stringify(formValues));
 }, 500));
 
@@ -18,12 +28,16 @@ if (json !== null) {
   const reload = JSON.parse(json);
   email.value = reload.email;
   message.value = reload.message;
+  if (message.value !== '' && email.value !== '') {
+     button.removeAttribute('disabled');
+  }
 };
+
 
 form.addEventListener('submit', (event) => {
   console.log(localStorage.getItem(storageKey));
   event.preventDefault();
   form.reset();
   localStorage.removeItem(storageKey);
+  button.setAttribute("disabled", "true");
 });
-  
